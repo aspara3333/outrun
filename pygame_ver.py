75d90496da30e5bc7683e581b197c1e90f06e859
pc-ryzen7 <syougot45@outlook.jp>
2023-06-23 02:23:21 +0900

    fix car class

--- pygame_ver.py
+++ pygame_ver.py
@@ -4,11 +4,21 @@ import random as rd
 import configparser
 
 car_ini=configparser.ConfigParser()
-car_ini.read('car.ini',encoding='utf-8')
+car_ini.read('./car.ini',encoding='UTF-8')
+
+window_size=WIDTH,HEIGHT=1000,700
+bg_color=(0,0,0)
+FPS=30
+
+pg.init()
+screen=pg.display.set_mode(window_size)
+pg.display.set_caption('outrun')
 
 class Car:
-    def __init__(self):
+    global car_ini
+    def __init__(self,model):
 #   初期化
+        self.model=model
         self.power=[]
         self.rpm=0
         self.kmh_be=0
@@ -25,14 +35,14 @@ class Car:
         self.rev=0
         self.weight=0
 #   車両データ読み込み
-        self.speed=int(car_ini[f'{self}']['speed'])
-        self.rev=int(car_ini[f'{self}']['rev']) 
+        self.speed=int(car_ini[f'{self.model}']['speed'])
+        self.rev=int(car_ini[f'{self.model}']['rev']) 
         for i in range(1,self.speed+1):
-            self.gear_ratio[i]=float(car_ini[f'{self}'][f'g{i}'])
-        self.final=float(car_ini[f'{self}']['final']) 
-        self.outer_cir=float(car_ini[f'{self}']['outer_cir'])
-        self.weight=int(car_ini[f'{self}']['weight'])
-    
+            self.gear_ratio.append(float(car_ini[f'{self.model}'][f'g{i}']))
+        self.final=float(car_ini[f'{self.model}']['final']) 
+        self.outer_cir=float(car_ini[f'{self.model}']['outer_cir'])
+        self.weight=int(car_ini[f'{self.model}']['weight'])
+#   車速計測
     def vehicle_spd(self):
         for i in range(0,int(self.rev/100)):
             if(int(self.rpm)<int(i+1)*100):
@@ -43,6 +53,21 @@ class Car:
         acc=((torque*self.gear*self.final)/(self.outer_cir*self.weight))
         self.kmh=self.kmh+acc*0.125
 
-rx8=Car()
+driving=Car('rx8')
+
+def main():
+#   画像読み込み
+
+    screen.fill(bg_color)
+    clock=pg.time.Clock()
+    while True:
+        
+        screen.fill(bg_color)
+        pg.display.update()
+        clock.tick(FPS)
+
+if(__name__=='__main__'):
+    main()
+    
 
         
