302cc2b2ebd5007e194b53b006dfc558e6d561aa
syogo tanaka <136896238+aspara3333@users.noreply.github.com>
2023-06-23 10:35:35 +0900

    fix meter class etc

--- pygame_ver.py
+++ pygame_ver.py
@@ -53,6 +53,16 @@ class Car:
         acc=((torque*self.gear*self.final)/(self.outer_cir*self.weight))
         self.kmh=self.kmh+acc*0.125
 
+class meter:
+    def __init__(self):
+#   画像読み込み(メーター系)
+        self.defi=pg.image.load('./image/defi_250.png')
+        self.defi_arrow=pg.image.load('./image/defi_arrow_250.png')
+
+    def write(self,rpm):
+        self.arrow_deg=rpm*0.03*-1
+        
+
 driving=Car('rx8')
 
 def main():
